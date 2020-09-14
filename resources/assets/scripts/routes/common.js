6ea609f6568d39669851ceab724b83b7747f4274
Daryna-dev <59832270+Daryna-dev@users.noreply.github.com>
2020-09-14 17:05:40 +0300

    hover

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,6 +3,20 @@ export default {
   init() {
     var headroom  = new Headroom(document.querySelector('nav'));
     headroom.init();
+    $('.nav__burger').click(function() {
+      $('.mobile-menu').toggleClass('open-mobile');
+      $('.shadow').toggleClass('show-shadow');
+      $('body').toggleClass('body-hidden');
+    });
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+      var div = $('.open-mobile'); // тут указываем ID элемента
+      if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        $('.mobile-menu').removeClass('open-mobile');
+        $('.shadow').removeClass('show-shadow');
+        $('body').removeClass('body-hidden');
+      }
+    });
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
