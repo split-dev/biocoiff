8debc204c6f7099c563e6c4f7f4e131e882f55ad
Daryna-dev <59832270+Daryna-dev@users.noreply.github.com>
2020-09-14 14:26:05 +0300

    page-hero

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,8 @@
+import Headroom from 'headroom.js'
 export default {
   init() {
-
+    var headroom  = new Headroom(document.querySelector('nav'));
+    headroom.init();
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
