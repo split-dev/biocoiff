c0bb7f87a92caea8294b7886768faf403af00235
Darina <dbogunenko@splitdev.pro>
2020-09-30 23:51:02 +0300

    products

--- resources/assets/scripts/routes/colours.js
+++ resources/assets/scripts/routes/colours.js
@@ -3,20 +3,23 @@ import Swiper from 'swiper/swiper-bundle';
 export default {
   init() {
     //eslint-disable-next-line no-unused-vars
-    var mySwiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-   /* let slide = false;
-    $('.btn-next').click(function () {
+    if ($('.colours-slider').hasClass('colours-slider')) {
+      var Swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    }
+   let slide = false;
+    $('.swiper-button-next').click(function () {
       if (slide) {
-        mySwiper.slideTo(0);
+        Swiper.slideTo(0);
       } else {
-        mySwiper.slideNext();
+        Swiper.slideNext();
       }
-    });*/
+    });
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
