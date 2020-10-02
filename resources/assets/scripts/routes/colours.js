f78e1e07f7d8235a79e7d0f3c7b204c50ec48ea4
Darina <dbogunenko@splitdev.pro>
2020-10-02 14:37:12 +0300

    slider

--- resources/assets/scripts/routes/colours.js
+++ resources/assets/scripts/routes/colours.js
@@ -5,28 +5,27 @@ export default {
     //eslint-disable-next-line no-unused-vars
     if ($('.colours-slider').hasClass('colours-slider')) {
       var mySwiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
       });
+      let slide = false;
+      $('.swiper-button-next').click(function () {
+        if (slide) {
+          mySwiper.slideTo(0);
+        } else {
+          mySwiper.slideNext();
+        }
+      });
+      $('.swiper-button-prev').click(function () {
+        if (slide) {
+          mySwiper.slideTo(0);
+        } else {
+          mySwiper.slidePrev();
+        }
+      });
     }
-   let slide = false;
-    $('.swiper-button-next').click(function () {
-      if (slide) {
-        mySwiper.slideTo(0);
-      } else {
-        mySwiper.slideNext();
-      }
-    });
-    $('.swiper-button-prev').click(function () {
-      if (slide) {
-        mySwiper.slideTo(0);
-      } else {
-        mySwiper.slidePrev();
-      }
-    });
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
